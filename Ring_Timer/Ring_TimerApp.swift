3e9a1b23dd9a84a56b71e554030075e88a064d81
Stanley Pan <imoment.developer@gmail.com>
2022-01-19 15:29:06 -0500

    Initial Commit

---
+++ Ring_Timer/Ring_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ring_TimerApp.swift
+//  Ring_Timer
+//
+//  Created by Stanley Pan on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct Ring_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
